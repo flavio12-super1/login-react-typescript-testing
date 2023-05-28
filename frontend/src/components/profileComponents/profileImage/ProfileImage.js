2e013ba3fccf11fbb7bf30c09b33b82954c7159e
flavio herrera <herreraflavio0@gmail.com>
2023-05-28 12:04:31 -0700

    more updates to profile

--- frontend/src/components/profileComponents/profileImage/ProfileImage.js
+++ frontend/src/components/profileComponents/profileImage/ProfileImage.js
@@ -1,6 +1,66 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "../../../styles/CommonStyles.css";
 import "./ProfileImage.css";
+
+const ImageArray = () => {
+  const initialImages = [
+    "https://i.pinimg.com/originals/d4/e0/13/d4e01341b8f4bdc193671689aaec2bbb.jpg",
+    "https://i.kym-cdn.com/entries/icons/facebook/000/035/767/cover4.jpg",
+    "https://i.ytimg.com/vi/UiCPytVT4bo/maxresdefault.jpg",
+    "https://yt3.googleusercontent.com/JVTJHpdwc5AR6ntZu96w-K0M44uLx93RUnUfSFaSMb-BL6cyw4T6ipXJOIpKNbBUQV0fdju7=s900-c-k-c0x00ffffff-no-rj",
+  ];
+
+  const [images, setImages] = useState(initialImages);
+
+  const removeImg = (index) => {
+    const updatedImages = [...images];
+    updatedImages.splice(index, 1); // Remove the image at the specified index
+
+    setImages(updatedImages);
+  };
+
+  return (
+    <div className="displayFlex">
+      {images.map((imageUrl, index) => (
+        <div className="userProfileImageDiv" key={index}>
+          <div>
+            <img src={imageUrl} alt="" className="userProfileImage" />
+          </div>
+          <div className="removeImg">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="35"
+              height="35"
+              cursor="pointer"
+              onClick={() => removeImg(index)} // Pass the index to removeImg function
+              viewBox="0 0 200 200"
+            >
+              <circle cx="100" cy="100" r="80" fill="red" />
+              <g transform="rotate(45 100 100)">
+                <line
+                  x1="60"
+                  y1="100"
+                  x2="140"
+                  y2="100"
+                  stroke="white"
+                  strokeWidth="12"
+                />
+                <line
+                  x1="100"
+                  y1="60"
+                  x2="100"
+                  y2="140"
+                  stroke="white"
+                  strokeWidth="12"
+                />
+              </g>
+            </svg>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
 
 function ProfileImage() {
   return (
@@ -42,28 +102,9 @@ function ProfileImage() {
           </div>
         </div>
       </div>
-      <div className="displayFlex">
-        <div className="userProfileImageDiv">
-          <img
-            src="https://i.pinimg.com/originals/d4/e0/13/d4e01341b8f4bdc193671689aaec2bbb.jpg"
-            alt=""
-            className="userProfileImage"
-          />
-        </div>
-        <div className="userProfileImageDiv">
-          <img
-            src="https://i.kym-cdn.com/entries/icons/facebook/000/035/767/cover4.jpg"
-            alt=""
-            className="userProfileImage"
-          />
-        </div>
-        <div className="userProfileImageDiv">
-          <img
-            src="https://i.ytimg.com/vi/UiCPytVT4bo/maxresdefault.jpg"
-            alt=""
-            className="userProfileImage"
-          />
-        </div>
+
+      <div>
+        <ImageArray />
       </div>
     </div>
   );
